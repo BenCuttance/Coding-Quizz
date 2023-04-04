755a5c5c179b7bbac898204652b19c047f7b5498
BenC <bcuttance1998@gmail.com>
2023-04-04 12:13:34 +0930

    linked js to html. Added working counter. Needs to add function to press btn

--- Js.js
+++ Js.js
@@ -2,16 +2,25 @@
 
 
 
+timeLeft = 90;
+
+function countdown(){
+
+timeLeft--;
+document.getElementById("counter").innerHTML = String( timeLeft );
+if (timeLeft > 0) {
+    setTimeout(countdown, 1000);
+}
+};
+ 
 
 
+// function recallHighscores()
 
+// function startButton()
 
-function recallHighscores()
+setTimeout(countdown, 1000);
 
-function startButton()
-
-function setTimer()
-
-function questionsPrompts()
+// function questionsPrompts()
 
 

2f55aa2124383ad012eab6c036cf587ce7d98082
BenC <bcuttance1998@gmail.com>
2023-04-04 17:59:39 +0930

    adding framework for questions and answers

--- Js.js
+++ Js.js
@@ -1,26 +1,67 @@
 
+var timerEl = document.getElementById("counter")
+var button = document.querySelector(".beginBtn")
+// var quizQuestions = [{
+//     questionString: "String values must be enclosed within ______ when being assinged to variables"
+//     choices: {
+//         correct: "Qoutes",
+//         wrong: ["Commas", "Curly Brackets","Parenthesis" ]
+//     }
+// }, {
+//     questionString: "Arrays in JavaScript can be used to store?"
+//     choices: {
+//         correct: "All of the above",
+//         wrong: ["Number and String", "Other Arrays","Booleans" ]
+//     }
+// }, {
+
+//     questionString: "Commonly used data types DO NOT include:"
+//     choices: {
+//         correct: "Alerts",
+//         wrong: ["Strings", "Booleans","Numbers"]
+//     }
+// }, {
+
+//     questionString: "The condition in an if / else statement is enclosed with _______"
+//     choices: {
+//         correct: "Curly Brackets",
+//         wrong: ["Quotes", "Parenthesis","Square Brackets"]
+
+//     }
+
+// }];
+
+// function recallHighscores/init()
 
 
-
-timeLeft = 90;
 
 function countdown(){
 
+var timeLeft = 90;  
+
+var timeInterval = setInterval(function () {
+
+if (timeLeft > 1){ 
+
+    timerEl.textContent ='Seconds remaining: ' + timeLeft; 
+
 timeLeft--;
-document.getElementById("counter").innerHTML = String( timeLeft );
-if (timeLeft > 0) {
-    setTimeout(countdown, 1000);
+
+} else if (timeLeft === 1){
+
+    timerEl.textContent = 'Seconds remaining: ' + timeLeft;
+    timeLeft--;
+}else {
+
+    timerEl.textContent = 'Seconds remaining: 0';
+        }
+    }, 1000);
 }
-};
- 
-
-
-// function recallHighscores()
-
-// function startButton()
-
-setTimeout(countdown, 1000);
 
 // function questionsPrompts()
 
+
+// Event Listeners
+
+button.addEventListener("click", countdown);
 

05416b810676c3c1f97978a0c9d93600e1cd5014
BenC <bcuttance1998@gmail.com>
2023-04-06 18:00:08 +0930

    added selectors for buttons and questions. Then added all iterations of questions and answers from arrays to buttons, Added event listers so if player presses correct answer the user is given next set of qutsions

--- Js.js
+++ Js.js
@@ -1,67 +1,143 @@
 
+// var currentQuestion0
 var timerEl = document.getElementById("counter")
 var button = document.querySelector(".beginBtn")
-// var quizQuestions = [{
-//     questionString: "String values must be enclosed within ______ when being assinged to variables"
-//     choices: {
-//         correct: "Qoutes",
-//         wrong: ["Commas", "Curly Brackets","Parenthesis" ]
-//     }
-// }, {
-//     questionString: "Arrays in JavaScript can be used to store?"
-//     choices: {
-//         correct: "All of the above",
-//         wrong: ["Number and String", "Other Arrays","Booleans" ]
-//     }
-// }, {
+var quizQuestions = [{
+    questionString: "String values must be enclosed within ______ when being assinged to variables",
 
-//     questionString: "Commonly used data types DO NOT include:"
-//     choices: {
-//         correct: "Alerts",
-//         wrong: ["Strings", "Booleans","Numbers"]
-//     }
-// }, {
+    choices: {
+        correct: "Qoutes",
+        wrong: ["Commas", "Curly Brackets", "Parenthesis"]
+    }
+}, {
+    questionString: "Arrays in JavaScript can be used to store?",
+    choices: {
+        correct: "All of the above",
+        wrong: ["Number and String", "Other Arrays", "Booleans"]
+    }
+}, {
 
-//     questionString: "The condition in an if / else statement is enclosed with _______"
-//     choices: {
-//         correct: "Curly Brackets",
-//         wrong: ["Quotes", "Parenthesis","Square Brackets"]
+    questionString: "Commonly used data types DO NOT include:",
+    choices: {
+        correct: "Alerts",
+        wrong: ["Strings", "Booleans", "Numbers"]
+    }
+}, {
 
-//     }
+    questionString: "The condition in an if / else statement is enclosed with _______",
+    choices: {
+        correct: "Curly Brackets",
+        wrong: ["Quotes", "Parenthesis", "Square Brackets"]
 
-// }];
+    }
+
+}];
 
 // function recallHighscores/init()
+console.log(quizQuestions[0].questionString)
+console.log(quizQuestions[0].choices.correct)
 
+function countdown() {
 
+    var timeLeft = 90;
 
-function countdown(){
+    var timeInterval = setInterval(function () {
 
-var timeLeft = 90;  
+        if (timeLeft > 1) {
 
-var timeInterval = setInterval(function () {
+            timerEl.textContent = 'Seconds remaining: ' + timeLeft;
 
-if (timeLeft > 1){ 
+            timeLeft--;
 
-    timerEl.textContent ='Seconds remaining: ' + timeLeft; 
+        } else if (timeLeft === 1) {
 
-timeLeft--;
+            timerEl.textContent = 'Seconds remaining: ' + timeLeft;
+            timeLeft--;
+        } else {
 
-} else if (timeLeft === 1){
-
-    timerEl.textContent = 'Seconds remaining: ' + timeLeft;
-    timeLeft--;
-}else {
-
-    timerEl.textContent = 'Seconds remaining: 0';
+            timerEl.textContent = 'Seconds remaining: 0';
         }
     }, 1000);
 }
 
-// function questionsPrompts()
+function changeDisplay() {
+
+    var beginBtn = document.querySelector('.beginBtn')
+    var questionContainer = document.querySelector(".container");
+    var answerGrid = document.querySelector('#answerButtons')
+
+    questionContainer.style.display = "block";
+
+    beginBtn.style.display = "none";
+
+}
+
+
+function beginQuiz() {
+    countdown()
+
+    changeDisplay()
+var quizBtn1 = document.querySelector('.btn1')
+var quizBtn2 = document.querySelector('.btn2')
+var quizBtn3 = document.querySelector('.btn3')
+var quizBtn4 = document.querySelector('.btn4')
+var questionSelector = document.querySelector('#question');
+
+questionSelector.textContent = quizQuestions[0].questionString;
+
+quizBtn1.textContent= quizQuestions[0].choices.wrong[0];
+quizBtn2.textContent = quizQuestions[0].choices.wrong[1];
+quizBtn3.textContent = quizQuestions[0].choices.correct;
+quizBtn4.textContent = quizQuestions[0].choices.wrong[2];
+
+quizBtn3.addEventListener('click', function () {
+
+questionSelector.textContent = quizQuestions[1].questionString;
+
+quizBtn1.textContent= quizQuestions[1].choices.wrong[0];
+quizBtn2.textContent = quizQuestions[1].choices.wrong[2];
+quizBtn3.textContent = quizQuestions[1].choices.wrong[1];
+quizBtn4.textContent = quizQuestions[1].choices.correct;
+
+quizBtn4.addEventListener('click', function (){
+
+questionSelector.textContent = quizQuestions[2].questionString;
+
+
+quizBtn1.textContent= quizQuestions[2].choices.correct;
+quizBtn2.textContent = quizQuestions[2].choices.wrong[2];
+quizBtn3.textContent = quizQuestions[2].choices.wrong[1];
+quizBtn4.textContent = quizQuestions[2].choices.wrong[0];
+
+quizBtn1.addEventListener('click', function (){
+
+questionSelector.textContent = quizQuestions[3].questionString;
+
+quizBtn1.textContent= quizQuestions[3].choices.wrong[1];
+quizBtn2.textContent = quizQuestions[3].choices.correct;
+quizBtn3.textContent = quizQuestions[3].choices.wrong[0];
+quizBtn4.textContent = quizQuestions[3].choices.wrong[2];
+
+quizBtn2.addEventListener('click', function (){
+
+questionSelector.textContent = "Score:" ;
+
+quizBtn1.style.display = "none"
+
+
+} )
+
+
+})
+
+})
+})
+}
+
+
 
 
 // Event Listeners
 
-button.addEventListener("click", countdown);
+button.addEventListener("click", beginQuiz);
 

1e6ede5b308a2e75cb8b04e025a9734dd9c655fc
BenC <bcuttance1998@gmail.com>
2023-04-09 19:29:42 +0930

    added clearer code for questions

--- Js.js
+++ Js.js
@@ -20,36 +20,42 @@ var quizQuestions = [{
 
     choices: {
         correct: "Qoutes",
-        wrong: ["Commas", "Curly Brackets", "Parenthesis"]
+        wrong: ["Commas", "Curly Brackets", "Parenthesis", "Qoutes"]
     }
 }, {
     questionString: "Arrays in JavaScript can be used to store?",
     choices: {
         correct: "All of the above",
-        wrong: ["Number and String", "Other Arrays", "Booleans"]
+        wrong: ["Number and String", "Other Arrays", "Booleans", "All of the above"]
     }
 }, {
 
     questionString: "Commonly used data types DO NOT include:",
     choices: {
         correct: "Alerts",
-        wrong: ["Strings", "Booleans", "Numbers"]
+        wrong: ["Alerts", "Strings", "Booleans", "Numbers", ]
     }
 }, {
 
     questionString: "The condition in an if / else statement is enclosed with _______",
     choices: {
         correct: "Curly Brackets",
-        wrong: ["Quotes", "Parenthesis", "Square Brackets"]
+        wrong: ["Quotes","Curly Brackets", "Parenthesis", "Square Brackets"]
 
-    }
+    }, 
+
+// }, {
+
+//         questionString: "Game Over, Score:", 
+    
+
 
 }];
 
 var timeLeft = 60;
 
 
-function stopTimer (){
+function stopTimer() {
     clearInterval(timeInterval);
 }
 
@@ -57,13 +63,9 @@ function stopTimer (){
 // function recallHighscores/init()
 
 
-
-
-
-
 var timeInterval = null;
 function countdown() {
-     timeInterval = setInterval(function () {
+    timeInterval = setInterval(function () {
 
         if (timeLeft > 1) {
 
@@ -78,7 +80,7 @@ function countdown() {
         } else {
 
             timerEl.textContent = "Seconds remaining: 0"
-            scoreForm ()
+            scoreForm()
             // timerEl.textContent = 'Seconds remaining: 0';
             // questionSelector.textContent = "Game Over!    Score: " + timeLeft;
             // quizBtn1.style.display = "none";
@@ -95,7 +97,7 @@ function countdown() {
 
 }
 
-
+var currentIndex = 0
 
 function changeDisplay() {
 
@@ -111,69 +113,98 @@ function changeDisplay() {
 
 }
 
+function checkAnswer(event){
+var correctAnswer = quizQuestions[currentIndex].choices.correct;
+var selectedAnswer = event.target.textContent;
+
+if ( correctAnswer == selectedAnswer) {
+    alert("Correct")
+} else { timeLeft -=15;
+    alert("wrong")
+}
+currentIndex++;
+displayQuestion ()
+
+
+}
+
+function displayQuestion () {
+
+    questionSelector.textContent = quizQuestions[currentIndex].questionString;
+
+    quizBtn1.textContent = quizQuestions[currentIndex].choices.wrong[0];
+    quizBtn2.textContent = quizQuestions[currentIndex].choices.wrong[1];
+    quizBtn3.textContent = quizQuestions[currentIndex].choices.wrong[2];
+    quizBtn4.textContent = quizQuestions[currentIndex].choices.wrong[3];
+
+   
+}
 
 function beginQuiz() {
-   
-   timerEl.style.display = "block" 
 
-   leaderBoard.style.display = "none"
-    
-   countdown()
+    timerEl.style.display = "block"
+
+    leaderBoard.style.display = "none"
+
+    countdown()
 
     changeDisplay()
 
 
-questionSelector.textContent = quizQuestions[0].questionString;
+    quizBtn1.addEventListener('click', checkAnswer)
 
-quizBtn1.textContent= quizQuestions[0].choices.wrong[0];
-quizBtn2.textContent = quizQuestions[0].choices.wrong[1];
-quizBtn3.textContent = quizQuestions[0].choices.correct;
-quizBtn4.textContent = quizQuestions[0].choices.wrong[2];
+    quizBtn2.addEventListener('click', checkAnswer)
 
+    quizBtn3.addEventListener('click', checkAnswer)
 
+    quizBtn4.addEventListener('click', checkAnswer)
 
-quizBtn3.addEventListener('click', function () {
-
-questionSelector.textContent = quizQuestions[1].questionString;
-
-quizBtn1.textContent= quizQuestions[1].choices.wrong[0];
-quizBtn2.textContent = quizQuestions[1].choices.wrong[2];
-quizBtn3.textContent = quizQuestions[1].choices.wrong[1];
-quizBtn4.textContent = quizQuestions[1].choices.correct;
-
-quizBtn4.addEventListener('click', function (){
-
-questionSelector.textContent = quizQuestions[2].questionString;
-
-
-quizBtn1.textContent= quizQuestions[2].choices.correct;
-quizBtn2.textContent = quizQuestions[2].choices.wrong[1];
-quizBtn3.textContent = quizQuestions[2].choices.wrong[2];
-quizBtn4.textContent = quizQuestions[2].choices.wrong[0];
-
-
-quizBtn1.addEventListener('click', function (){
-
-questionSelector.textContent = quizQuestions[3].questionString;
-
-quizBtn1.textContent= quizQuestions[3].choices.wrong[1];
-quizBtn2.textContent = quizQuestions[3].choices.correct;
-quizBtn3.textContent = quizQuestions[3].choices.wrong[0];
-quizBtn4.textContent = quizQuestions[3].choices.wrong[2];
-
-quizBtn2.addEventListener('click', function (){
-
-    scoreForm() 
+    displayQuestion()
 
 
 
-} )
+    // quizBtn3.addEventListener('click', function () {
+
+    //     questionSelector.textContent = quizQuestions[1].questionString;
+
+    //     quizBtn1.textContent = quizQuestions[1].choices.wrong[0];
+    //     quizBtn2.textContent = quizQuestions[1].choices.wrong[2];
+    //     quizBtn3.textContent = quizQuestions[1].choices.wrong[1];
+    //     quizBtn4.textContent = quizQuestions[1].choices.correct;
+
+    //     quizBtn4.addEventListener('click', function () {
+
+    //         questionSelector.textContent = quizQuestions[2].questionString;
 
 
-})
+    //         quizBtn1.textContent = quizQuestions[2].choices.correct;
+    //         quizBtn2.textContent = quizQuestions[2].choices.wrong[1];
+    //         quizBtn3.textContent = quizQuestions[2].choices.wrong[2];
+    //         quizBtn4.textContent = quizQuestions[2].choices.wrong[0];
 
-})
-})
+
+    //         quizBtn1.addEventListener('click', function () {
+
+    //             questionSelector.textContent = quizQuestions[3].questionString;
+
+    //             quizBtn1.textContent = quizQuestions[3].choices.wrong[1];
+    //             quizBtn2.textContent = quizQuestions[3].choices.correct;
+    //             quizBtn3.textContent = quizQuestions[3].choices.wrong[0];
+    //             quizBtn4.textContent = quizQuestions[3].choices.wrong[2];
+
+    //             quizBtn2.addEventListener('click', function () {
+
+    //                 scoreForm()
+
+
+
+    //             })
+
+
+    //         })
+
+    //     })
+    // })
 }
 
 // Event Listeners
@@ -181,49 +212,32 @@ quizBtn2.addEventListener('click', function (){
 button.addEventListener("click", beginQuiz);
 clearInterval(timeInterval);
 
-function scoreForm () {
+function scoreForm() {
 
     stopTimer()
-   
+
     timerEl.textContent = "Seconds remaining:" + timeLeft;
-    questionSelector.textContent = "Game Over: Score: " + timeLeft  ;
+    questionSelector.textContent = "Game Over: Score: " + timeLeft;
     leaderBoard.style.display = "block"
-    
+
     retryBtn.style.display = "block"
     quizBtn1.style.display = "none";
     quizBtn2.style.display = "none";
     quizBtn3.style.display = "none";
     quizBtn4.style.display = "none";
-    
-// submitBtn.addEventListener('click', function(){
 
-// localStorage.setItem("fname", collectName.value)
+    // submitBtn.addEventListener('click', function(){
 
-// })
+    // localStorage.setItem("fname", collectName.value)
+
+    // })
 
 
 }
 
 
-// quizBtn1.addEventListener('click', function () {
 
-//     timeLeft -= 5;
-    
-//     })
-    
-//     quizBtn2.addEventListener('click', function () {
-    
-//         timeLeft -= 5;
-        
-//         })
-    
-//     quizBtn4.addEventListener('click', function () {
-    
-//             timeLeft -= 5;
-            
-//             })
-
-function retryQuiz () {
+function retryQuiz() {
 
     quizBtn1.style.display = "block";
     quizBtn2.style.display = "block";
@@ -232,22 +246,11 @@ function retryQuiz () {
 
     timeLeft = 60;
 
-beginQuiz ()
+    beginQuiz()
 
 
 }
 
-retryBtn.addEventListener('click', retryQuiz )
-
-// function setThree () {
-
-// questionSelector.textContent = quizQuestions[2].questionString;
+retryBtn.addEventListener('click', retryQuiz)
 
 
-// quizBtn1.textContent= quizQuestions[2].choices.correct;
-// quizBtn2.textContent = quizQuestions[2].choices.wrong[1];
-// quizBtn3.textContent = quizQuestions[2].choices.wrong[2];
-// quizBtn4.textContent = quizQuestions[2].choices.wrong[0];
-
-
-// }
